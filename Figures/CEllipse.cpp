06fbf98c99456263009ecacc0aeabeab28019323
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-01-24 15:09:40 +0200

    finsished loading all the shapes

--- Figures/CEllipse.cpp
+++ Figures/CEllipse.cpp
@@ -1,4 +1,5 @@
 #include "CEllipse.h"
+#include <fstream>
 CEllipse::CEllipse(): CFigure(){}
 CEllipse::CEllipse(Point P1, int len, GfxInfo FigureGfxInfo) :CFigure(FigureGfxInfo)
 {
@@ -8,6 +9,34 @@ CEllipse::CEllipse(Point P1, int len, GfxInfo FigureGfxInfo) :CFigure(FigureGfxI
 
 void CEllipse::Load(ifstream& inputFile)
 {
+	string borderColor, fillColor;
+	//int borderWidth; //to read the width of each figure's border
+	inputFile >> ID >> center.x>> center.y >> radius;
+
+
+
+	inputFile >> borderColor;
+	this->FigGfxInfo.DrawClr = StringToColor(borderColor);
+
+	inputFile >> fillColor;
+	//cout << borderColor<< endl << fillColor << endl;
+
+	if (fillColor == "NO-FILL") {
+		//cout << fillColor << endl;
+		this->FigGfxInfo.isFilled = false;
+	}
+	else
+	{
+		this->FigGfxInfo.isFilled = true;
+		this->FigGfxInfo.FillClr = StringToColor(fillColor);
+	}
+	/*inputFile >> borderWidth;
+	this->FigGfxInfo.BorderWdth = borderWidth;*/
+
+
+	//inputFile >> this->FigGfxInfo.Resize_Factor;
+	Selected = false;
+	this->setStored();
 }
 
 void CEllipse::DrawMe(GUI* pGUI) const
--- Figures/CHex.cpp
+++ Figures/CHex.cpp
@@ -1,4 +1,5 @@
 #include "CHex.h"
+#include <fstream>
 CHex::CHex() : CFigure() {}
 CHex::CHex(Point P1, int len, int rlen, GfxInfo FigureGfxInfo) :CFigure(FigureGfxInfo)
 {
@@ -7,7 +8,36 @@ CHex::CHex(Point P1, int len, int rlen, GfxInfo FigureGfxInfo) :CFigure(FigureGf
 	Rlen = rlen;
 }
 
-void CHex::Load(ifstream& inputFile) {}
+void CHex::Load(ifstream& inputFile) {
+	string borderColor, fillColor;
+	int borderWidth; //to read the width of each figure's border
+	inputFile >> ID >> TopLeft.x >> TopLeft.y >> Llen >> Rlen;
+
+
+
+	inputFile >> borderColor;
+	this->FigGfxInfo.DrawClr = StringToColor(borderColor);
+
+	inputFile >> fillColor;
+	//cout << borderColor<< endl << fillColor << endl;
+
+	if (fillColor == "NO-FILL") {
+		//cout << fillColor << endl;
+		this->FigGfxInfo.isFilled = false;
+	}
+	else
+	{
+		this->FigGfxInfo.isFilled = true;
+		this->FigGfxInfo.FillClr = StringToColor(fillColor);
+	}
+	/*inputFile >> borderWidth;
+	this->FigGfxInfo.BorderWdth = borderWidth;*/
+
+
+	//inputFile >> this->FigGfxInfo.Resize_Factor;
+	Selected = false;
+	this->setStored();
+} 
 void CHex::DrawMe(GUI* pGUI) const
 {
 	//Call Output::DrawPolygon to draw a Hex on the screen	
