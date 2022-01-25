e146c4c9035717e7bec606730a0d65da6464237f
Nada-Mamdouh <nada-mamdouh@outlook.com>
2022-01-25 14:47:10 +0200

    solved conflicts

--- Figures/CFigure.cpp
+++ Figures/CFigure.cpp
@@ -43,7 +43,31 @@ color CFigure::StringToColor(string s) {
 		return TURQUOISE;
 	return BLACK;
 }
-
+string CFigure::ColorToString(color c) {
+	if ((c.ucBlue == BLACK.ucBlue) && (c.ucGreen == BLACK.ucGreen) && (c.ucRed == BLACK.ucRed))
+		return "BLACK";
+	if ((c.ucBlue == BLUE.ucBlue) && (c.ucGreen == BLUE.ucGreen) && (c.ucRed == BLUE.ucRed))
+		return "BLUE";
+	if ((c.ucBlue == WHITE.ucBlue) && (c.ucGreen == WHITE.ucGreen) && (c.ucRed == WHITE.ucRed))
+		return "WHITE";
+	if ((c.ucBlue == RED.ucBlue) && (c.ucGreen == RED.ucGreen) && (c.ucRed == RED.ucRed))
+		return "RED";
+	if ((c.ucBlue == YELLOW.ucBlue) && (c.ucGreen == YELLOW.ucGreen) && (c.ucRed == YELLOW.ucRed))
+		return "YELLOW";
+	if ((c.ucBlue == GREEN.ucBlue) && (c.ucGreen == GREEN.ucGreen) && (c.ucRed == GREEN.ucRed))
+		return "GREEN";
+	if ((c.ucBlue == LIGHTGOLDENRODYELLOW.ucBlue) && (c.ucGreen == LIGHTGOLDENRODYELLOW.ucGreen) && (c.ucRed == LIGHTGOLDENRODYELLOW.ucRed))
+		return "LIGHTGOLDENRODYELLOW";
+	if ((c.ucBlue == MAGENTA.ucBlue) && (c.ucGreen == MAGENTA.ucGreen) && (c.ucRed == MAGENTA.ucRed))
+		return "MAGENTA";
+	if ((c.ucBlue == TURQUOISE.ucBlue) && (c.ucGreen == TURQUOISE.ucGreen) && (c.ucRed == TURQUOISE.ucRed))
+		return "TURQUOISE";
+	if ((c.ucBlue == SKYBLUE.ucBlue) && (c.ucGreen == SKYBLUE.ucGreen) && (c.ucRed == SKYBLUE.ucRed))
+		return "SKYBLUE";
+	if ((c.ucBlue == LIGHTSTEELBLUE.ucBlue) && (c.ucGreen == LIGHTSTEELBLUE.ucGreen) && (c.ucRed == LIGHTSTEELBLUE.ucRed))
+		return "LIGHTSTEELBLUE";
+	return "NO-FILL";
+}
 void CFigure::setStored()
 {
 	//getting data from the stored file
