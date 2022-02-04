3912ba6f144eff0146568807ee54e7a3e9f7bef1
Amr Elauoty <amr.ehab.elauoty@gmail.com>
2022-02-05 01:23:18 +0200

    restored

--- Actions/ActionSelect.cpp
+++ Actions/ActionSelect.cpp
@@ -1,59 +1,90 @@
 #include "ActionSelect.h"
-
+#include <iostream>
+#include "ActionPlayWithColors.h"
+#include "ActionPlayWithShapes.h"
 #include "..\ApplicationManager.h"
-
-
+#include <string>
+#include <iostream>
 ActionSelect::ActionSelect(ApplicationManager* pApp) :Action(pApp) ///constructor chaining
-{}
+{
+	selectedEllipses = 0;
+	selectedHexagons = 0;
+	selectedSquares = 0;
+}
 
 void ActionSelect::Execute()
 {
-	int cx;
-	int cy;
+	int* selectedFigCount = pManager->getSelectedFigCount();
+	if (UI.InterfaceMode == MODE_DRAW || UI.InterfaceMode == MODE_COLOR || UI.InterfaceMode == MODE_FILL_COLOR) {
+		int cx;
+		int cy;
 
-	GUI* pGUI = pManager->GetGUI();
+		GUI* pGUI = pManager->GetGUI();
 
-	pGUI->GetPointClicked(cx, cy);
+		pGUI->GetPointClicked(cx, cy);
 
-	CFigure* fig = pManager->GetFigure(cx,cy);
-	if (UI.InterfaceMode != MODE_PLAY)
-	{
+		CFigure* fig = pManager->GetFigure(cx, cy);
+
+
+
+
 		if (fig != NULL) {
 
 			if (fig->IsSelected()) {
 				fig->ChngDrawClr(fig->ChngSelectClr());
 				fig->SetSelected(false);
+				*selectedFigCount = *selectedFigCount - 1;
 				pGUI->ClearStatusBar();
 			}
 			else {
 				fig->ChngSelectClr(fig->GetCurrentDrawClr());
-				fig->ChngDrawClr(MAGENTA);
+				fig->ChngDrawClr(CADETBLUE);
 				fig->SetSelected(true);
+				*selectedFigCount = *selectedFigCount - 1;
 				pGUI->PrintMessage(fig->ShowFigureDetails());
+
 
 			}
 		}
 		else {
 
-			CFigure* selectedFigure = pManager->GetSelectedFigure();
-			if (selectedFigure != NULL) {
-				selectedFigure->ChngDrawClr(selectedFigure->ChngSelectClr());
-				selectedFigure->SetSelected(false);
+			CFigure* selectedFiguer = pManager->GetSelectedFigure();
+			if (selectedFiguer != NULL) {
+				selectedFiguer->ChngDrawClr(selectedFiguer->ChngSelectClr());
+				selectedFiguer->SetSelected(false);
 				pGUI->ClearStatusBar();
 			}
 		}
 	}
-	else
-	{
+	else if (UI.InterfaceMode == MODE_PLAY) {
+		///selecting in playmode to delete the shape after selection
+		int cx;
+		int cy;
 
-		if (fig != NULL) {
+		GUI* pGUI = pManager->GetGUI();
 
-			fig->ChngSelectClr(fig->GetCurrentDrawClr());
-			fig->ChngDrawClr(MAGENTA);
-			fig->SetSelected(true);
-			Action* newAct = new ActionDelete(pManager);
-			newAct->Execute();
+		pGUI->GetPointClicked(cx, cy);
+
+		CFigure* fig = pManager->GetFigure(cx, cy);
+
+		if (fig != NULL)
+		{
+			if (pManager->getSelectedCasePlay() == 1)
+			{
+				Action* newAct = new ActionPlayWithShapes(pManager);
+			}
+			else if (pManager->getSelectedCasePlay() == 2)
+			{
+				Action* newAct = new ActionPlayWithColors(pManager);
+			}
+			else if (pManager->getSelectedCasePlay() == 3)
+			{
+				// Implement your Action here and put this statment inside it
+				//pManager->setSelectedCasePlay(3);
+				// Load a file at the beginnig of the function (You can see playwithcolors action to see what i mean)
+			}
+
 		}
 	}
-	
+
 }
